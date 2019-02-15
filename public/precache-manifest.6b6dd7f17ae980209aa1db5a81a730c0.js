065d4d98558500e5b4bfb36cd75d409edf46a09e
William Cruz <chiefxcruz@gmail.com>
2019-02-15 01:11:01 -0600

    Ready to publish

---
+++ public/precache-manifest.6b6dd7f17ae980209aa1db5a81a730c0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ab10d000cdf7ee45a57395feee160ad2",
+    "url": "/static/media/logo.ab10d000.png"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "9b99abb09d0a3603d737",
+    "url": "/static/js/main.9b99abb0.chunk.js"
+  },
+  {
+    "revision": "6b4798a642b2a8ebd556",
+    "url": "/static/js/2.6b4798a6.chunk.js"
+  },
+  {
+    "revision": "9b99abb09d0a3603d737",
+    "url": "/static/css/main.7670908a.chunk.css"
+  },
+  {
+    "revision": "119ce6003d38f892d34b15295d749fe9",
+    "url": "/index.html"
+  }
+];
